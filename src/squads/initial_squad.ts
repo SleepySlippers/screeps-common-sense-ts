1879906bc82e1a421cedc1a49d5955f1332284f7
arseny staroverov <arsen2001god@bk.ru>
2023-03-04 03:36:59 +0300

    make squad staff prettier

--- src/squads/initial_squad.ts
+++ src/squads/initial_squad.ts
@@ -1,30 +1,18 @@
 import { NAME_SEPARATOR } from "roles/role";
-import { SpawConstructibleSquad, Squad } from "./squad_loader";
+import { SpawnConstructibleSquad, Squad } from "./squad_loader";
 import { GenerateSquadList, SpawnOrOperateSquad, SpawnTemplate } from "./squad_utils";
 
 const SPAWN_SEQUENCE: SpawnTemplate[] = [
     // TODO: hardcode SPAWN_SEQUENCE
 ]
 
-export default class InitialSquad implements Squad, SpawConstructibleSquad {
-    spawner!: StructureSpawn;
-    readonly squad_name: string = "initial_squad";
-    readonly squad_prefix = this.spawner + NAME_SEPARATOR + this.squad_name + "#";
-
-    construct_from_spawner(spawner: StructureSpawn): Squad {
-        let res = new InitialSquad();
-        res.spawner = spawner;
-        return res;
+export class InitialSquad extends SpawnConstructibleSquad {
+    constructor(){
+        super("initial_squad", SPAWN_SEQUENCE);
     }
 
     IsActive(): boolean {
         // TODO: turn off this squad when can handle cooler squad
-        return true;
+        return true
     }
-    Operate(): void {
-        const is_active = this.IsActive();
-        let squad_list = GenerateSquadList(this, SPAWN_SEQUENCE);
-        SpawnOrOperateSquad(this.spawner, squad_list, is_active);
-    }
-
 }
