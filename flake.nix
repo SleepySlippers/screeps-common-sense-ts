46825c6fbd73e13fcdbeff4dbf0bc1c63d8256e2
Arseny Staroverov <arsen2001god@bk.ru>
2026-02-02 01:37:30 +0300

    Add nix staff

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Dev shell with old nixpkgs and rollup";
+
+  inputs = {
+    # Pick a channel / revision that still has the rollup you want
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystemPassThrough (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.${system}.default = (import ./default.nix {
+          inherit pkgs;
+          system = "${system}";
+          nodejs = pkgs.nodejs-12_x;
+        }).shell;
+      }
+    );
+}
